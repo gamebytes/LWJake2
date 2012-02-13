4e9282128036f270695ef11efa0aedf517b5ff2a
Ethan Lee <flibitijibibo@gmail.com>
2012-02-13 01:37:47 -0500

    More progress on CTF... this might take a while...

--- src/lwjake2/game/CTF.java
+++ src/lwjake2/game/CTF.java
@@ -75,7 +75,7 @@ public class CTF {
 	public static final int CTF_TEAM_BONUS = 10; // what your team gets for capture
 	public static final int CTF_RECOVERY_BONUS = 1; // what you get for recovery
 	public static final int CTF_FLAG_BONUS = 0; // what you get for picking up enemy flag
-	public static final int CTF_FLAG_CARRIER_BONUS = 2; // what you get for fragging enemy flag carrier
+	public static final int CTF_FRAG_CARRIER_BONUS = 2; // what you get for fragging enemy flag carrier
 	public static final int CTF_FLAG_RETURN_TIME = 40; // seconds until auto return
 	
 	public static final int CTF_CARRIER_DANGER_PROTECT_BONUS = 2; // bonus for fraggin someone who has recently hurt your flag carrier
@@ -268,8 +268,8 @@ public class CTF {
 	=================
 	*/
 	public static edict_t[] loc_findradius(edict_t from[], float[] org, float rad) {
-		float[]	eorg;
-		int		j;
+		float[]	eorg = new float[3];
+		int j;
 		
 		
 		if (from == null)
@@ -310,7 +310,7 @@ public class CTF {
 		trace_t	trace;
 		float[][] targpoints = new float[8][3];
 		int i;
-		float[] viewpoint;
+		float[] viewpoint = new float[3];
 
 		// bmodels need special checking because their origin is 0,0,0
 		if (targ.movetype == Defines.MOVETYPE_PUSH)
@@ -407,9 +407,6 @@ public class CTF {
 	
 	/*--------------------------------------------------------------------------*/
 	
-	// public static edict_t SelectRandomDeathmatchSpawnPoint();
-	// public static edict_t SelectFarthestDeathmatchSpawnPoint();
-	
 	public static void CTFAssignSkin(edict_t ent, String s) {
 		int playernum = ent.index - GameBase.g_edicts.length - 1;
 
@@ -430,8 +427,290 @@ public class CTF {
 		// gi.cprintf(ent, PRINT_HIGH, "You have been assigned to %s team.\n", ent->client->pers.netname);
 	}
 	
-	public static void CTFAssignTeam(gclient_t who);
-	public static edict_t SelectCTFSpawnPoint (edict_t ent);
+	public static void CTFAssignTeam(gclient_t who) {
+		edict_t player;
+		int i;
+		int team1count = 0, team2count = 0;
+
+		who.resp.ctf_state = 0;
+
+		if (((int) GameBase.dmflags.value & DF_CTF_FORCEJOIN) == 0) {
+			who.resp.ctf_team = CTF_NOTEAM;
+			return;
+		}
+
+		for (i = 1; i <= GameBase.maxclients.value; i++) {
+			player = GameBase.g_edicts[i];
+
+			if (!player.inuse || player.client == who)
+				continue;
+
+			switch (player.client.resp.ctf_team) {
+			case CTF_TEAM1:
+				team1count++;
+				break;
+			case CTF_TEAM2:
+				team2count++;
+			}
+		}
+		if (team1count < team2count)
+			who.resp.ctf_team = CTF_TEAM1;
+		else if (team2count < team1count)
+			who.resp.ctf_team = CTF_TEAM2;
+		else if (Math.random() > 0.5)
+			who.resp.ctf_team = CTF_TEAM1;
+		else
+			who.resp.ctf_team = CTF_TEAM2;
+	}
+	
+	/*
+	================
+	SelectCTFSpawnPoint
+
+	go to a ctf point, but NOT the two points closest
+	to other players
+	================
+	*/
+	public static edict_t SelectCTFSpawnPoint (edict_t ent) {
+		EdictIterator spot;
+		edict_t	spot1, spot2;
+		int	count = 0;
+		int	selection;
+		float range, range1, range2;
+		String cname;
+
+		if (ent.client.resp.ctf_state != 0)
+			if (((int) GameBase.dmflags.value & Defines.DF_SPAWN_FARTHEST) == 0)
+				return PlayerClient.SelectFarthestDeathmatchSpawnPoint();
+			else
+				return PlayerClient.SelectRandomDeathmatchSpawnPoint();
+
+		ent.client.resp.ctf_state++;
+
+		switch (ent.client.resp.ctf_team) {
+		case CTF_TEAM1:
+			cname = "info_player_team1";
+			break;
+		case CTF_TEAM2:
+			cname = "info_player_team2";
+			break;
+		default:
+			return PlayerClient.SelectRandomDeathmatchSpawnPoint();
+		}
+
+		spot = null;
+		range1 = range2 = 99999;
+		spot1 = spot2 = null;
+
+		while ((spot = GameBase.G_Find(spot, GameBase.findByClass, cname)) != null) {
+			count++;
+			range = PlayerClient.PlayersRangeFromSpot(spot.o);
+			if (range < range1)
+			{
+				range1 = range;
+				spot1 = spot.o;
+			}
+			else if (range < range2)
+			{
+				range2 = range;
+				spot2 = spot.o;
+			}
+		}
+
+		if (count == 0)
+			return PlayerClient.SelectRandomDeathmatchSpawnPoint();
+
+		if (count <= 2)
+			spot1 = spot2 = null;
+		else
+			count -= 2;
+
+		selection = (int) (Math.random() * count);
+
+		spot = null;
+		do {
+			spot = GameBase.G_Find(spot, GameBase.findByClass, cname);
+			if (spot.o == spot1 || spot.o == spot2)
+				selection++;
+		} while (selection-- != 0);
+
+		return spot.o;
+	}
+	
+	/*------------------------------------------------------------------------*/
+	
+	/*
+	CTFFragBonuses
+
+	Calculate the bonuses for flag defense, flag carrier defense, etc.
+	Note that bonuses are not cumaltive.  You get one, they are in importance
+	order.
+	*/
+	public static void CTFFragBonuses(edict_t targ, edict_t inflictor, edict_t attacker) {
+		int i;
+		edict_t ent;
+		gitem_t flag_item, enemy_flag_item;
+		int otherteam;
+		EdictIterator flag;
+		edict_t carrier = null;
+		String c;
+		float[] v1 = null, v2 = null;
+
+		if (targ.client != null && attacker.client != null) {
+			if (attacker.client.resp.ghost != null)
+				if (attacker != targ)
+					attacker.client.resp.ghost.kills++;
+			if (targ.client.resp.ghost != null)
+				targ.client.resp.ghost.deaths++;
+		}
+
+		// no bonus for fragging yourself
+		if (targ.client == null || attacker.client == null || targ == attacker)
+			return;
+
+		otherteam = CTFOtherTeam(targ.client.resp.ctf_team);
+		if (otherteam < 0)
+			return; // whoever died isn't on a team
+
+		// same team, if the flag at base, check to he has the enemy flag
+		if (targ.client.resp.ctf_team == CTF_TEAM1) {
+			flag_item = flag1_item;
+			enemy_flag_item = flag2_item;
+		} else {
+			flag_item = flag2_item;
+			enemy_flag_item = flag1_item;
+		}
+
+		// did the attacker frag the flag carrier?
+		if (targ.client.pers.inventory[GameItems.ITEM_INDEX(enemy_flag_item)] != 0) {
+			attacker.client.resp.ctf_lastfraggedcarrier = GameBase.level.time;
+			attacker.client.resp.score += CTF_FRAG_CARRIER_BONUS;
+			GameBase.gi.cprintf(attacker, Globals.PRINT_MEDIUM,
+				"BONUS: " + CTF_FRAG_CARRIER_BONUS + "points for fragging enemy flag carrier.\n");
+
+			// the target had the flag, clear the hurt carrier
+			// field on the other team
+			for (i = 1; i <= GameBase.maxclients.value; i++) {
+				ent = GameBase.g_edicts[i];
+				if (ent.inuse && ent.client.resp.ctf_team == otherteam)
+					ent.client.resp.ctf_lasthurtcarrier = 0;
+			}
+			return;
+		}
+
+		if (targ.client.resp.ctf_lasthurtcarrier > 0.0f &&
+			GameBase.level.time - targ.client.resp.ctf_lasthurtcarrier < CTF_CARRIER_DANGER_PROTECT_TIMEOUT &&
+			attacker.client.pers.inventory[GameItems.ITEM_INDEX(flag_item)] == 0) {
+			// attacker is on the same team as the flag carrier and
+			// fragged a guy who hurt our flag carrier
+			attacker.client.resp.score += CTF_CARRIER_DANGER_PROTECT_BONUS;
+			GameBase.gi.bprintf(Globals.PRINT_MEDIUM,
+				attacker.client.pers.netname + " defends " +
+				CTFTeamName(attacker.client.resp.ctf_team) + "'s flag carrier against an agressive enemy\n"
+			);
+			if (attacker.client.resp.ghost != null)
+				attacker.client.resp.ghost.carrierdef++;
+			return;
+		}
+
+		// flag and flag carrier area defense bonuses
+
+		// we have to find the flag and carrier entities
+
+		// find the flag
+		switch (attacker.client.resp.ctf_team) {
+		case CTF_TEAM1:
+			c = "item_flag_team1";
+			break;
+		case CTF_TEAM2:
+			c = "item_flag_team2";
+			break;
+		default:
+			return;
+		}
+
+		flag = null;
+		while ((flag = GameBase.G_Find(flag, GameBase.findByClass, c)) != null) {
+			if ((flag.o.spawnflags & Defines.DROPPED_ITEM) == 0)
+				break;
+		}
+
+		if (flag == null)
+			return; // can't find attacker's flag
+
+		// find attacker's team's flag carrier
+		for (i = 1; i <= GameBase.maxclients.value; i++) {
+			carrier = GameBase.g_edicts[i];
+			if (carrier.inuse && 
+				carrier.client.pers.inventory[GameItems.ITEM_INDEX(flag_item)] != 0)
+				break;
+			carrier = null;
+		}
+
+		// ok we have the attackers flag and a pointer to the carrier
+
+		// check to see if we are defending the base's flag
+		Math3D.VectorSubtract(targ.s.origin, flag.o.s.origin, v1);
+		Math3D.VectorSubtract(attacker.s.origin, flag.o.s.origin, v2);
+
+		if ((Math3D.VectorLength(v1) < CTF_TARGET_PROTECT_RADIUS ||
+				Math3D.VectorLength(v2) < CTF_TARGET_PROTECT_RADIUS ||
+				loc_CanSee(flag.o, targ) || loc_CanSee(flag.o, attacker)) &&
+				attacker.client.resp.ctf_team != targ.client.resp.ctf_team) {
+			// we defended the base flag
+			attacker.client.resp.score += CTF_FLAG_DEFENSE_BONUS;
+			if (flag.o.solid == Defines.SOLID_NOT)
+				GameBase.gi.bprintf(Globals.PRINT_MEDIUM,
+					attacker.client.pers.netname + " defends the " +
+					CTFTeamName(attacker.client.resp.ctf_team) + " base.\n"
+				);
+			else
+				GameBase.gi.bprintf(Globals.PRINT_MEDIUM,
+					attacker.client.pers.netname + " defends the " +
+					CTFTeamName(attacker.client.resp.ctf_team) + " flag.\n" 
+				);
+			if (attacker.client.resp.ghost != null)
+				attacker.client.resp.ghost.basedef++;
+			return;
+		}
+
+		if (carrier != null && carrier != attacker) {
+			Math3D.VectorSubtract(targ.s.origin, carrier.s.origin, v1);
+			Math3D.VectorSubtract(attacker.s.origin, carrier.s.origin, v1);
+
+			if (Math3D.VectorLength(v1) < CTF_ATTACKER_PROTECT_RADIUS ||
+					Math3D.VectorLength(v2) < CTF_ATTACKER_PROTECT_RADIUS ||
+				loc_CanSee(carrier, targ) || loc_CanSee(carrier, attacker)) {
+				attacker.client.resp.score += CTF_CARRIER_PROTECT_BONUS;
+				GameBase.gi.bprintf(Globals.PRINT_MEDIUM,
+					attacker.client.pers.netname + " defends the " + 
+					CTFTeamName(attacker.client.resp.ctf_team) + "'s flag carrier.\n"
+				);
+				if (attacker.client.resp.ghost != null)
+					attacker.client.resp.ghost.carrierdef++;
+				return;
+			}
+		}
+	}
+
+	public static void CTFCheckHurtCarrier(edict_t targ, edict_t attacker) {
+		gitem_t flag_item;
+
+		if (targ.client == null || attacker.client == null)
+			return;
+
+		if (targ.client.resp.ctf_team == CTF_TEAM1)
+			flag_item = flag2_item;
+		else
+			flag_item = flag1_item;
+
+		if (targ.client.pers.inventory[GameItems.ITEM_INDEX(flag_item)] != 0 &&
+			targ.client.resp.ctf_team != attacker.client.resp.ctf_team)
+			attacker.client.resp.ctf_lasthurtcarrier = GameBase.level.time;
+	}
+	
+	/*------------------------------------------------------------------------*/
+	
 	public static boolean CTFPickup_Flag(edict_t ent, edict_t other);
 	public static boolean CTFDrop_Flag(edict_t ent, gitem_t item);
 	public static void CTFEffects(edict_t player);
@@ -444,8 +723,6 @@ public class CTF {
 	public static void CTFSay_Team(edict_t who, String msg);
 	public static void CTFFlagSetup (edict_t ent);
 	public static void CTFResetFlag(int ctf_team);
-	public static void CTFFragBonuses(edict_t targ, edict_t inflictor, edict_t attacker);
-	public static void CTFCheckHurtCarrier(edict_t targ, edict_t attacker);
 
 	// GRAPPLE
 	public static void CTFWeapon_Grapple (edict_t ent);
@@ -455,8 +732,8 @@ public class CTF {
 
 	//TECH
 	public static gitem_t CTFWhat_Tech(edict_t ent);
-	public static boolean CTFPickup_Tech (edict_t ent, edict_t *other);
-	public static void CTFDrop_Tech(edict_t ent, gitem_t *item);
+	public static boolean CTFPickup_Tech (edict_t ent, edict_t other);
+	public static void CTFDrop_Tech(edict_t ent, gitem_t item);
 	public static void CTFDeadDropTech(edict_t ent);
 	public static void CTFSetupTechSpawn();
 	public static int CTFApplyResistance(edict_t ent, int dmg);
