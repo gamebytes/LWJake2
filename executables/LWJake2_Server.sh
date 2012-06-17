e91408ad6fa70488bd190f1c485e34cb103617a7
Ethan Lee <flibitijibibo@gmail.com>
2012-06-17 05:58:29 -0300

    Update master

--- executables/LWJake2_Server.sh
+++ executables/LWJake2_Server.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# Move to script's directory
 cd `dirname $0`
+
+# Get the kernel information
+UNAME=`uname`
+
+# Set the proper libpath
+if [ "$UNAME" == "Darwin" ]; then
+  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:./
+elif [ "$UNAME" == "Linux" ]; then
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
+fi
+
+# Run the server!
 java -jar lwjake2.jar +set dedicated 1 $@
